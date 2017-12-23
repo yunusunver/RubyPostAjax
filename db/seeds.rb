555fd5304609c1788ed33f315c55afd848814a87
Yunus Unver <unver4434@hotmail.com>
2017-12-23 13:23:49 +0000

    Ruby Projesi

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+(1..10).each do |p|
+    Post.create(title: p.to_s,msg: "deneme")
+end
